5d866fe18747207bba6731532c26f561b3d617e7
MashfiqulHoque1 <1921648@iub.edu.bd>
2021-12-21 23:52:47 +0600

    third commit

--- meal.js
+++ meal.js
@@ -1,37 +1,71 @@
+//Showing data form api
+function api () {
 
-function api()
-{
     var typed = document.getElementById("textbox").value;
-    var url = `https://www.themealdb.com/api/json/v1/1/categories.php`;
+ 
+ fetch (`https://www.themealdb.com/api/json/v1/1/search.php?s=${typed}`)
+ .then (res => res.json() )
+ .then (data => showData(data.meals) ); 
+ }
+ 
+ api ();
 
-    fetch(url)
-    .then (res => res.json())
-    .then (data => showData(data.categories));
+ function showData (arraydata) {
+     console.log(arraydata);
+ 
+     var content = document.getElementById("grid-container");
+ 
+  for (var i=0; i<arraydata.length; i++){
+ 
+     var newDiv = document.createElement("div");
+     newDiv.classList.add("api-data-style")
+     newDiv.innerHTML = `#Info:- ${i+1} <br>
+     <div class="card" style="width: 23rem;">
+         <img src="${arraydata[i].strMealThumb}" class="card-img-top" alt="...">
+         <div class="card-body">
+           <h5 class="card-title">${arraydata[i].strMeal}</h5>
+           <p class="card-text">${arraydata[i].strInstructions}</p>
+           <a href="#" class="btn btn-primary">Go Anywhere</a>
+         </div>
+   </div>`;
+         content.appendChild(newDiv);
+         
+ }
 }
 
-api();
 
-function showData(arraydata)
-{
-    console.log(arraydata);
+//After searching data
+function searchResults() {
+    var typed = document.getElementById("textbox").value;
+  
+  fetch (`https://www.themealdb.com/api/json/v1/1/search.php?s=${typed}`)
+        .then(res => res.json())
+        .then(data => SearchCard(data.meals))
+ }
+ 
+ function SearchCard(data) {
+    var element = document.getElementById("grid-container");
+ 
+    var content = document.getElementById("search-api-data");
+    element.parentNode.removeChild(element);
+ 
+    var newDiv = document.createElement("div");
+    newDiv.classList.add("api-data-style")
+    for (var i = 0; i<25; i++) {
+ 
+       var typed = document.getElementById("textbox").value;
+        if (typed == data[i].strMeal) {
 
-    var content = document.getElementById("grid-container");
-
-    for(var i = 0; i <= arraydata.length; i++)
-    {
-        var newDiv = document.createElement("div");
-        newDiv.classList.add("api-data-style");
-        newDiv.innerHTML = `
-            #Info:- ${i+1} <br>
-            <div class="card" style="width: 23rem;">
-                <img src="${arraydata[i].strCategoryThumb}" class="card-img-top" alt="...">
-                <div class="card-body">
-                  <h5 class="card-title">${arraydata[i].strCategory}</h5>
-                  <p class="card-text">${arraydata[i].strCategoryDescription}</p>
-                  <a href="#" class="btn btn-primary">Go Anywhere</a>
-                </div>
-          </div>`
-            content.appendChild(newDiv);
+           newDiv.innerHTML = `#Info:- ${i+1} <br>
+           <div class="card" style="width: 23rem;">
+               <img src="${data[i].strMealThumb}" class="card-img-top" alt="...">
+               <div class="card-body">
+                 <h5 class="card-title">${data[i].strMeal}</h5>
+                 <p class="card-text">${data[i].strInstructions}</p>
+                 <a href="#" class="btn btn-primary">Go Anywhere</a>
+               </div>
+         </div>`;
+         content.appendChild(newDiv);
+        }
     }
-    
-}
+ }
